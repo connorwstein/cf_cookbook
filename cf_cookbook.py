6702db4b34f66472168c2442fc775b673f8256a2
connorwstein <connor.stein@mail.mcgill.ca>
2016-08-28 20:03:28 +0000

    Added login/signup to menu bar

--- cf_cookbook.py
+++ cf_cookbook.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
-
 Bootstrap(app)
-@app.route('/')
-def hello_world():
-    return render_template('index.html') 
 
-@app.route('/test')
-def hello_world_2():
-    return render_template('test.html') 
+@app.route('/')
+def get_home():
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     Bootstrap(app)
